ea0fe449e04562e2a1fdbc26a72290e82f3bdd86
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-09-06 16:52:15 +0200

    added view

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import os
 from django.shortcuts import render
+from django.conf.settings import PROJECT_ROOT
 
-# Create your views here.
+class MovieListView(request):
+	"""listing the movies"""
+	model = Movie
+	def getAllMovies(self, arg):
+		moviesFile = open(os.path.join(PROJECT_ROOT, 'movies.txt'))
+		moviesList =moviesFile.readlines()
+		for m in moviesList:
+			print (m)
+		return render(request,'movies/movie-list.html')  
